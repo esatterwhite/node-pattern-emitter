22c448c923e1d0317db0ba487178671c0b8d7c34
Eric Satterwhite <esatterwhite@wi.rr.com>
2014-05-04 10:11:15 -0500

    initial smoke test

---
+++ test/emitter.js
@@ -0,0 +1,11 @@
+var assert = require("assert")
+  , PatterEmitter = require("../lib")
+
+
+describe('PatternEmitter', function(){
+	describe("#addListener", function(){
+		it('should pass', function(){
+			assert.ok(1);
+		});
+	});
+});
