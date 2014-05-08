8dab9a87b9036f5c0ace617ffb0b502720451f2e
Eric Satterwhtie <esatterwhite@wi.rr.com>
2014-05-08 09:03:43 -0500

    adding some more tests around removing events

--- test/emitter.removeListener.js
+++ test/emitter.removeListener.js
@@ -6,15 +6,57 @@ describe("PatternEmitter", function(){
 	describe("#removeListener", function( ){
 		it("should remove a handler by name", function(){
 			var emitter = new PatternEmitter();
+
 			var fn = function(){
-				assert.ok( false )
+				throw new Error();
 			}
 
 			emitter.addListener('test', fn)
-
+			emitter.addListener('test2', fn)
 			emitter.removeListener('test', fn )
-			emitter.emit('test')
-			assert.ok( true )
-		})
+
+			assert.doesNotThrow(function(){
+				emitter.emit('test')
+			});
+
+			assert.throws(function(){
+				emitter.emit('test2')
+			});
+		});
+
+	});
+
+	describe("#removeAllListeners", function(){
+		it('should remove all handlers', function(){
+			var emitter = new PatternEmitter();
+			var fn = function(){
+				throw new Error()
+			}
+
+			emitter.addListener('test', fn)
+			emitter.addListener('test2', fn)
+
+			assert.throws(function(){
+				emitter.emit( 'test' )
+			})
+
+			assert.throws(function(){
+				emitter.emit( 'test2' )
+			})
+
+			emitter.removeAllListeners();
+
+			assert.doesNotThrow(function(){
+				emitter.emit( 'test' )
+			})
+
+			assert.doesNotThrow(function(){
+				emitter.emit( 'test2' )
+			})
+
+
+		});
+		
 	})
-})
+
+});
